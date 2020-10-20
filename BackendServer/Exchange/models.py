e3ae9f822bc81d52429fe1bde5f80fd05db3b901
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2020-10-20 11:00:34 +0200

    PEP 8 corrections

--- BackendServer/Exchange/models.py
+++ BackendServer/Exchange/models.py
@@ -3,16 +3,19 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE, related_name = 'profile')
-    balance = models.DecimalField(max_digits = 12, decimal_places = 2, default = 1000.00)
-    created = models.DateTimeField(auto_now_add = True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    balance = models.DecimalField(max_digits=12, decimal_places=2, default=1000.00)
+    created = models.DateTimeField(auto_now_add=True)
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
@@ -20,20 +23,24 @@ def save_user_profile(sender, instance, **kwargs):
 
 
 class Company(models.Model):
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length=50)
+
     class Meta:
         verbose_name_plural = "companies"
 
+
 class Stock(models.Model):
-    company = models.ForeignKey('Company', on_delete = models.CASCADE, related_name = 'stocks')
-    name = models.CharField(max_length = 50, default = "")
-    price = models.DecimalField(max_digits = 12, decimal_places = 2, default = 0.00)
-    avail_amount = models.PositiveIntegerField(default = 0)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE, related_name='stocks')
+    name = models.CharField(max_length=50, default="")
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    avail_amount = models.PositiveIntegerField(default=0)
+
 
 class UserStock(models.Model):
-    user = models.ForeignKey('Profile', on_delete = models.CASCADE, related_name = 'userstocks')
-    stock = models.ForeignKey('Stock', on_delete = models.CASCADE, related_name = 'userstocks')
-    stock_amount = models.PositiveIntegerField(default = 0)
+    user = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='userstocks')
+    stock = models.ForeignKey('Stock', on_delete=models.CASCADE, related_name='userstocks')
+    stock_amount = models.PositiveIntegerField(default=0)
+
 
 class BuyOffer(models.Model):
     OPEN = 1
@@ -44,13 +51,14 @@ class BuyOffer(models.Model):
         (EXPIRED, 'expired'),
         (CLOSED, 'closed')
     ]
-    
-    user = models.ForeignKey('Profile', on_delete = models.CASCADE, related_name = 'buyoffers')
-    stock = models.ForeignKey('Stock', on_delete = models.CASCADE, related_name = 'buyoffers')
-    unit_price = models.DecimalField(max_digits = 12, decimal_places = 2, default = 0.00)
-    status = models.PositiveSmallIntegerField(choices = STATUS_TYPES, default = OPEN)
-    stock_amount = models.PositiveIntegerField(default = 0)
-    created = models.DateTimeField(auto_now_add = True)
+
+    user = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='buyoffers')
+    stock = models.ForeignKey('Stock', on_delete=models.CASCADE, related_name='buyoffers')
+    unit_price = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    status = models.PositiveSmallIntegerField(choices=STATUS_TYPES, default=OPEN)
+    stock_amount = models.PositiveIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+
 
 class SellOffer(models.Model):
     OPEN = 1
@@ -61,17 +69,18 @@ class SellOffer(models.Model):
         (EXPIRED, 'expired'),
         (CLOSED, 'closed')
     ]
-    
-    user_stock = models.ForeignKey('UserStock', on_delete = models.CASCADE, related_name = 'selloffers')
-    unit_price = models.DecimalField(max_digits = 12, decimal_places = 2)
-    stock_amount = models.PositiveIntegerField(default = 0)
-    status = models.PositiveSmallIntegerField(choices = STATUS_TYPES, default = OPEN)
-    created = models.DateTimeField(auto_now_add = True)
+
+    user_stock = models.ForeignKey('UserStock', on_delete=models.CASCADE, related_name='selloffers')
+    unit_price = models.DecimalField(max_digits=12, decimal_places=2)
+    stock_amount = models.PositiveIntegerField(default=0)
+    status = models.PositiveSmallIntegerField(choices=STATUS_TYPES, default=OPEN)
+    created = models.DateTimeField(auto_now_add=True)
+
 
 class Transaction(models.Model):
-    sell = models.ForeignKey('SellOffer', on_delete = models.CASCADE, related_name = 'transactions', null = True)
-    buy = models.ForeignKey('BuyOffer', on_delete = models.CASCADE, related_name = 'transactions', null = True)
-    user = models.ForeignKey('Profile', on_delete = models.CASCADE, related_name = 'transactions', null = True)
-    amount = models.PositiveIntegerField(default = 0)
-    unit_price = models.DecimalField(max_digits = 12, decimal_places = 2)
-    date = models.DateTimeField(auto_now_add = True)
+    sell = models.ForeignKey('SellOffer', on_delete=models.CASCADE, related_name='transactions', null=True)
+    buy = models.ForeignKey('BuyOffer', on_delete=models.CASCADE, related_name='transactions', null=True)
+    user = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='transactions', null=True)
+    amount = models.PositiveIntegerField(default=0)
+    unit_price = models.DecimalField(max_digits=12, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
