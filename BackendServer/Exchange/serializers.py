24bc6105131ae443ccfb4255ac358c08a756edb2
bayar96 <zawadzki123m@gmail.com>
2020-10-22 13:50:01 +0200

    added model imports

--- BackendServer/Exchange/serializers.py
+++ BackendServer/Exchange/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
+        model = Profile
         fields = ('id', 'balance', 'created')
 
 class UserDetailSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer()
     class Meta:
+        model = User
         fields = ('id', 'username', 'email', 'groups', 'profile',
                   'first_name', 'last_name')
         read_only_fields = ('profile', 'groups')
