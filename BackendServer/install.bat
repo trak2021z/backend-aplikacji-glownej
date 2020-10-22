c90fc4750301811608b0aea4b111c66437674555
Bartosz Maziarka <barmazprzgit@gmail.com>
2020-10-22 21:10:01 +0200

    Fixed seed_stocks.py - now generating non-integer values

--- BackendServer/install.bat
+++ BackendServer/install.bat
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py loaddata companies
-python manage.py createsuperuser
 python manage.py shell < seed_stocks.py
+python manage.py createsuperuser
