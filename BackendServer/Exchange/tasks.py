93f758d4c1894eba1780e19ad5f79727b3c9ab37
Bartosz Maziarka <barmazprzgit@gmail.com>
2020-10-23 15:29:30 +0200

    Added task for executing buy/sell offers

--- BackendServer/Exchange/tasks.py
+++ BackendServer/Exchange/tasks.py
@@ -3,6 +3,7 @@ from .celery import app
 from .models import *
 from decimal import *
 from random import *
+from datetime import datetime as dt
 
 MAX_STOCKS = 1000
 
@@ -44,3 +45,62 @@ def regenerate_stocks():
         new_items_count = stock.avail_amount + randrange(MAX_STOCKS - stock.avail_amount)
         stock.avail_amount = new_items_count
         stock.save()
+
+
+@app.task
+def match_sell_buy_offers():
+    buy_offers = BuyOffer.objects.all().filter(status=1)
+    sell_offers = SellOffer.objects.all().filter(status=1)
+    for buy_offer in buy_offers:
+        stock = buy_offer.stock
+        profile = buy_offer.user.profile
+        balance = profile.balance
+        if buy_offer.unit_price > stock.price:
+            if (stock.avail_amount < buy_offer.stock_amount):
+                total_price = stock.avail_amount * stock.price
+                if (total_price > balance):
+                    amount = int(balance / stock.price)
+                else:
+                    amount = stock.avail_amount
+            else:
+                amount = buy_offer.stock_amount
+            stock.avail_amount -= amount
+            buy_offer.stock_amount -= amount
+            if buy_offer.stock_amount == 0:
+                buy_offer.status = 3
+            buy_offer.save()
+            stock.save()
+            profile.balance -= amount * stock.price
+            profile.save()
+            new_transaction = Transaction(
+                sell=None,
+                buy=buy_offer,
+                stock=stock,
+                user=None,
+                amount=amount,
+                unit_price=stock.price,
+                date=dt.now,
+                is_sell=False,
+            )
+            new_transaction.save()
+    for sell_offer in sell_offers:
+        stock = sell_offer.stock
+        profile = sell_offer.user.profile
+        if sell_offer.unit_price < stock.price:
+            stock.avail_amount += sell_offer.stock_amount
+            profile.balance += sell_offer.stock_amount * stock.price
+            stock.save()
+            profile.save()
+            sell_offer.status = 3
+            sell_offer.save()
+            new_transaction = Transaction(
+                sell=sell_offer,
+                buy=None,
+                stock=stock,
+                user=None,
+                amount=sell_offer.stock_amount,
+                unit_price=stock.price,
+                date=dt.now,
+                is_sell=True,
+            )
+            new_transaction.save()
