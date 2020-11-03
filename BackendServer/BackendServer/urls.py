19210f2a82e112903ef99cf9ff8e9786da622b0d
Bartosz Maziarka <barmazprzgit@gmail.com>
2020-11-03 01:39:57 +0100

    Added user deletion endpoint

--- BackendServer/BackendServer/urls.py
+++ BackendServer/BackendServer/urls.py
@@ -56,6 +56,7 @@ urlpatterns = [
     url(r'^user/stocks/$', v.UserStockView.as_view(), name='user_stocks_view'),
     url(r'^user/wallet/$', v.UserWalletView.as_view(), name='user_wallet_view'),
     url(r'^user/history/$', v.UserTransactionView.as_view(), name='user_history_view'),
+    url(r'^user/delete/$', v.UserDeleteView.as_view(), name='user_delete_view'),
     url(r'^user/offers/$', v.UserOffersView.as_view({'get': 'get'}), name='user_offers_view'),
     url(r'^stocks/(?P<pk>\d+)/buy/$', v.StockBuyView.as_view(), name='stock_buy_view'),
     url(r'^transaction/(?P<pk>\d+)/$', v.TransactionView.as_view(), name='transaction_stock_view'),
